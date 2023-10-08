c31b22824079058038dc4f79669be3bdacc09700
m.sredzinski94@gmail.com <m.sredzinski94@gmail.com>
2023-10-08 15:39:04 +0200

    Set up controller

--- RabbitMqSender/Controllers/RabbitSenderController.cs
+++ RabbitMqSender/Controllers/RabbitSenderController.cs
@@ -1,15 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RabbitMqSender.Services;
 
-namespace RabbitMqSender.Controllers
-{
-    public class RabbitSenderController : Controller
+namespace RabbitMqSender.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class RabbitSenderController : Controller
+{   
+    [HttpGet]
+    public IActionResult Send()
     {
-        public IActionResult Send()
-        {
-            MessageSenderService messageSenderService = new MessageSenderService();
-            messageSenderService.Send();
-            return Ok();
-        }      
-    }
+        MessageSenderService messageSenderService = new MessageSenderService();
+        messageSenderService.Send();
+        return Ok();
+    }      
 }
