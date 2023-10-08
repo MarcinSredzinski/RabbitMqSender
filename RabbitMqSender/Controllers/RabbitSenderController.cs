9574bcecc83cc64d8f58fccf74660602f873cef1
m.sredzinski94@gmail.com <m.sredzinski94@gmail.com>
2023-10-08 15:16:05 +0200

    modivied service

---
+++ RabbitMqSender/Controllers/RabbitSenderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMqSender.Services;
+
+namespace RabbitMqSender.Controllers
+{
+    public class RabbitSenderController : Controller
+    {
+        public IActionResult Send()
+        {
+            MessageSenderService messageSenderService = new MessageSenderService();
+            messageSenderService.Send();
+            return Ok();
+        }      
+    }
+}
--- RabbitMqSender/Services/MessageSenderService.cs
+++ RabbitMqSender/Services/MessageSenderService.cs
@@ -1,13 +1,37 @@
 ﻿using RabbitMQ.Client;
+using System.Text;
+using System.Threading.Channels;
 
 namespace RabbitMqSender.Services
 {
     public class MessageSenderService
     {
+
+        private readonly string exchangeName = "DemoExchange";
+        private readonly string routingKey = "demo-routing-key";
+        private readonly string queueName = "DemoQueue";
+        private readonly IModel? channel;
         public MessageSenderService()
         {
             ConnectionFactory factory = new();
-            factory.Uri = new Uri("anqp://guest:guest@localhost:5672");
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            factory.ClientProvidedName = "Rabbit Sender App";
+            IConnection connection = factory.CreateConnection();
+
+            if (connection != null)
+            {
+                channel = connection.CreateModel();
+                channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+                channel.QueueDeclare(queueName, false, false, false, null);
+                channel.QueueBind(queueName, exchangeName, routingKey, null);
+            }
+        }
+
+        public void Send()
+        {
+            byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello world");
+
+            channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
         }
     }
 }
